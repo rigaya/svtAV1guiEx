b8cc9adaad9177e1f07f7f28c13e5596a1bbcc7c
rigaya <rigaya34589@live.jp>
2022-03-19 18:17:43 +0900

    音声のみ出力時のエラーを修正。

--- svtAV1guiEx/encode/auo_encode.cpp
+++ svtAV1guiEx/encode/auo_encode.cpp
@@ -401,8 +401,8 @@ BOOL check_output(CONF_GUIEX *conf, const OUTPUT_INFO *oip, const PRM_ENC *pe, g
 
     //必要な実行ファイル
     //x264
-    if (!conf->oth.disable_guicmd) {
-        if (pe->video_out_type != VIDEO_OUTPUT_DISABLED && !PathFileExists(exstg->s_enc.fullpath)) {
+    if (!conf->oth.disable_guicmd && pe->video_out_type != VIDEO_OUTPUT_DISABLED) {
+        if (!PathFileExists(exstg->s_enc.fullpath)) {
             const auto targetExes = find_target_exe_files("SvtAv1Enc", exeFiles);
             if (targetExes.size() > 0) {
                 const auto latestVidEnc = find_latest_svtav1(targetExes);
